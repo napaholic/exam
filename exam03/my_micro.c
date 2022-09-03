686a4a87162daf508d14b60e0835d0dc2ad4c270
napaholic <jaewkim@student.42seoul.kr>
2022-09-03 17:09:29 +0900

    asdf

--- exam03/my_micro.c
+++ exam03/my_micro.c
@@ -31,5 +31,44 @@ int chk_pos(float x, float y, float id_x, float id_y, float w, float h)
 {
 	if (x < id_x || x > id_x + w || y < id_y || y > id_y + h)
 		return (0);
+	else if (x - id_x < 1.000000 || (id_x + w) - x < 1.000000 || y - id_y < 1.000000
+	|| (id_y + h) - y < 1.000000)
+		return (1);
+	return (2);
+}
+
+int main(int ac, char **av)
+{
+	FILE *file;
+	char *paper;
+	int read, pos;
+	int y, x;
+	int b_width, b_height;
+	char background;
+	char id, color;
+	float id_x, id_y, w, h;
 	
+	if (ac != 2)
+		return (ft_perror(ERR1));
+	if (!(file = fopen(av[1], "r")) || (fscanf(file, "%d %d %c\n", &b_width, &b_height, &background) != 3) || \
+	(!(b_width > 0 && b_width <= 300 && b_height > 0 && b_height < 300)) || \
+	(!(paper = (char *)malloc(sizeof(char) * (b_width * b_height)))))
+	return (ft_perror(ERR1));
+	memset(paper, background, b_width * b_height);
+	while ((read = fscanf(file, "%c %f %f %f %f %c\n", &id, &id_x, &id_y, &w, &h, &color)) == 6)
+	{
+		if (!(w > 0 && h > 0) || !(id == 'R' || id == 'r'))
+			break;
+		y = -1;
+		while (++y < b_height)
+		{
+			x = -1;
+			while (++x < b_width)
+			{
+				pos = chk_pos((float)x, (float)y, id_x, id_y, w, h);
+				if (pos == 1 || (pos == 2 && id == 'R'))
+					paper[y * b_width + x] = color;
+			}
+		}
+	}
 }
---
+++ exam03/mymicro.c
@@ -0,0 +1,53 @@
+//
+// Created by jaewkim on 2022/08/31.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define ERR_argc "ERROR: argc count error"
+
+int ft_strlen(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i])
+	{
+		++i;
+	}
+	return i;
+}
+
+int print_err(char *str)
+{
+	write(1, str, ft_strlen(str));
+	return 0;
+}
+
+int main(int ac, char **av)
+{
+	FILE *file;
+	char *paper;
+	int read, pos;
+	int b_w, b_h;
+	char color, id, background;
+	float id_x, id_y, id_w, id_h;
+	int
+	
+	if (ac != 2)
+		return print_err(ERR_argc);
+	if (!(file = fopen(av[1], "r")))
+		return print_err("ERROR : file dir error");
+	if (fscanf(file, "%d %d %c\n", &b_w, &b_h, &background) != 3)
+		return print_err("ERR: background data is wrong");
+	if (!(b_w > 0 && b_w <= 300) || !(b_h > 0 && b_h <= 300) || !(paper = (char *)malloc(sizeof(char) * (b_w * b_h))))
+		return (print_err("ERR : background data is wrong"));
+	memset(paper, background, b_w * b_h);
+	while((read = fscanf(file, "%c %f %f %f %f %c\n", &id, &id_x, &id_y, &id_w, &id_h, &color)) == 6)
+	{
+		if (!(id_w > 0 && id_h > 0) || !(id == 'r' || id == 'R'))
+			break;
+	}
+	
+}
