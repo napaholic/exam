3a492c15fbb21c982dc3d9de6ef9ab61d773ef74
Jaewook Kim <jaewkim@c4r7s5.42seoul.kr>
2022-09-05 17:53:32 +0900

    asdf

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/mjay/goinfre/exam
+# Install script for directory: /goinfre/jaewkim/exam
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/mjay/goinfre/exam/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/goinfre/jaewkim/exam/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- exam03/my_micro.c
+++ exam03/my_micro.c
@@ -33,3 +33,35 @@ int chk_pos(float x, float y, float id_x, float id_y, float w, float h)
 		return (0);
 	
 }
+
+int main(int ac, char **av)
+{
+    char id_c, r, background;
+    int b_w, b_h;
+    int id_w, id_h, id_x, id_y;
+    int pos
+    FILE *file;
+    char *paper;
+    int read, pos;
+    int x, y;
+
+    if (ac != 2)
+        return (ft_perror("argument count is error"));
+    if (!(file = fopen(av[1], "r")))
+        return (ft_perror("file dir is wrong"));
+    if ((fscanf(file, "%d %d %c\n", &b_w, &b_h), &background) != 3)
+        return (ft_perror("background data is wrong"));
+    if (!(b_w > 0 && b <= 300 && b_h > 0 && b_h <= 300))
+        return (ft_perror("background data overflow"));
+    if (!(paper = (char *)malloc(sizeof(char) * (b_w * b_h))))
+        return (ft_perror("malloc error"));
+    memset(paper, background, b_w * b_h);
+    while ((read = fscanf(file, "%c %f %f %f %f %c\n", &id, &id_x, &id_y, &id_w, &id_h, &id_c)) == 6)
+    {
+
+    }
+    if (read != -1) {
+        free(paper);
+        return (ft_perror("read fail : read pointer is not the end of file"));
+    }
+}
